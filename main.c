30a9e57ae95273b144f38c458a66592943aa332a
Tyler Jones <tdjones879@gmail.com>
2017-08-31 19:31:05 -0600

    Refactor and add error handling

--- main.c
+++ main.c
@@ -1,23 +1,56 @@
 #include <stdio.h>
+#include <stdint.h>
 
 /**
- * Return the number of expected bytes for the current codepoint.
- * @param ch Pointer to first byte of UTF-8 character, modified to contain correct MSB
+ * Attempt to decode the framing of the leading UTF8 byte
+ *
+ * @param  ch Pointer to first byte of UTF-8 character, modified to contain correct MSB
+ * @return    Number of bytes for the current codepoint, -1 if invalid
  */
-int get_utf_len(unsigned char *ch)
+uint32_t get_utf_len(unsigned char *ch)
 {
-    if ((*ch & 0xF0) == 0xF0) {
+    /* We only want to match with the exact headers */
+    /* Valid UTF8 must always have a zero bit following
+       the framing bits ex. 111>0< XXXX */
+    if ((*ch & 0xF8) == 0xF0) {
         *ch &= ~0xF8;
         return 3;
-    } else if ((*ch & 0xE0) == 0xE0) {
+    } else if ((*ch & 0xF0) == 0xE0) {
         *ch &= ~0xF0;
         return 2;
-    } else if ((*ch & 0xC0) == 0xC0) {
+    } else if ((*ch & 0xE0) == 0xC0) {
         *ch &= ~0xE0;
         return 1;
-    } else {
+    } else if ((*ch & 0x80) != 0x80) {
         *ch &= ~0x80;
         return 0;
+    } else {
+        return -1;
+    }
+}
+
+/**
+ * Print all valid decoded UTF8 codepoints within a file
+ */
+void decode_utf(FILE *file)
+{
+    uint32_t utf = 0;
+    unsigned char ch = 0;
+    int i, bytes = 0;
+    while(!feof(file)) {
+        ch = fgetc(file);
+        if (ch == '\n')
+            continue;
+
+        bytes = get_utf_len(&ch);
+        utf = ch;
+        for (i = 0; i < bytes; i++) {
+            utf <<= 6;
+            ch = fgetc(file) & ~0xC0;
+            utf |= ch;
+        }
+	if (bytes >= 0)
+            printf("U+%x\n", utf);
     }
 }
 
@@ -25,9 +58,6 @@ int main(int argc, char **argv)
 {
     FILE *file;
     char *file_name;
-    int i, bytes = 0;
-    unsigned int utf = 0;
-    unsigned char ch = 0;
     
     if (argv[1])
         file_name = argv[1];
@@ -37,21 +67,9 @@ int main(int argc, char **argv)
     file = fopen(file_name, "r");
 
     if (file) {
-        while(!feof(file)) {
-            ch = fgetc(file);
-            if (ch == '\n')
-                return 0;
-
-            bytes = get_utf_len(&ch);
-            utf = ch;
-            for (i = 0; i < bytes; i++) {
-                utf <<= 6;
-                ch = fgetc(file) & ~0xC0;
-                utf |= ch;
-            }
-            printf("U+%x\n", utf);
-        }
+        decode_utf(file);
         fclose(file);
     }
+
     return 0;
 }
